287dd3be2fe01a998109d108653031ae2f47996d
Clare Liguori <liguori@amazon.com>
2019-11-01 09:50:16 -0700

    Initial action code (#1)
    Initial action code
    
    Co-Authored-By: Steve Winton <stevewinton+notifications-github@gmail.com>

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 Inserts a container image URI into an Amazon ECS task definition file, creating a new task definition file.
 
+## Usage
+
+To insert the image URI `amazon/amazon-ecs-sample:latest` as the image for the `web` container in the task definition file, and then deploy the edited task definition file to ECS:
+
+```yaml
+    - name: Render Amazon ECS task definition
+      id: render-web-container
+      uses: aws/amazon-ecs-render-task-definition-for-github-actions@release
+      with:
+        task-definition: task-definition.json
+        container-name: web
+        image: amazon/amazon-ecs-sample:latest
+
+    - name: Deploy to Amazon ECS service
+      uses: aws/amazon-ecs-deploy-task-definition-for-github-actions@release
+      with:
+        task-definition: ${{ steps.render-web-container.outputs.task-definition }}
+        service: my-service
+        cluster: my-cluster
+```
+
 ## License Summary
 
 This code is made available under the MIT license.
---
+++ index.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const core = require('@actions/core');
+const tmp = require('tmp');
+const fs = require('fs');
+
+async function run() {
+  try {
+    // Get inputs
+    const taskDefinitionFile = core.getInput('task-definition', { required: true });
+    const containerName = core.getInput('container-name', { required: true });
+    const imageURI = core.getInput('image', { required: true });
+
+    // Parse the task definition
+    const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
+      taskDefinitionFile :
+      path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
+    if (!fs.existsSync(taskDefPath)) {
+      throw new Error(`Task definition file does not exist: ${taskDefinitionFile}`);
+    }
+    const taskDefContents = require(taskDefPath);
+
+    // Insert the image URI
+    if (!Array.isArray(taskDefContents.containerDefinitions)) {
+      throw new Error('Invalid task definition format: containerDefinitions section is not present or is not an array');
+    }
+    const containerDef = taskDefContents.containerDefinitions.find(function(element) {
+      return element.name == containerName;
+    });
+    if (!containerDef) {
+      throw new Error('Invalid task definition: Could not find container definition with matching name');
+    }
+    containerDef.image = imageURI;
+
+    // Write out a new task definition file
+    var updatedTaskDefFile = tmp.fileSync({
+      dir: process.env.RUNNER_TEMP,
+      prefix: 'task-definition-',
+      postfix: '.json',
+      keep: true,
+      discardDescriptor: true
+    });
+    const newTaskDefContents = JSON.stringify(taskDefContents, null, 2);
+    fs.writeFileSync(updatedTaskDefFile.name, newTaskDefContents);
+    core.setOutput('task-definition', updatedTaskDefFile.name);
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
+}
