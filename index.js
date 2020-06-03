5efb2d69de8c427cc5ccc262e9ed769c39f82116
Andrew-Chen-Wang <acwang02@gmail.com>
2020-06-02 21:14:06 -0400

    Added initial code for handling secrets

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ async function run() {
     const taskDefinitionFile = core.getInput('task-definition', { required: true });
     const containerName = core.getInput('container-name', { required: true });
     const imageURI = core.getInput('image', { required: true });
+    const secrets = core.getInput('secrets', { required: false });
 
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
@@ -30,6 +31,23 @@ async function run() {
       throw new Error('Invalid task definition: Could not find container definition with matching name');
     }
     containerDef.image = imageURI;
+
+    // Insert any secrets from Parameter Store or Secrets Manager.
+    // Up to here, we know that the containerDefinition must exist
+    if (secrets && secrets.toLowerCase() === 'true') {
+        // Check the task definition if secrets section exists
+        if (!Array.isArray(containerDef.secrets)) {
+            throw new Error('Invalid task definition format: secrets section must be an array')
+        }
+        // Loop through array of, hopefully, dictionaries
+        containerDef.secrets.forEach(element => {
+            const valueFrom = element.valueFrom(":")
+            if (valueFrom.length != 2) {
+                throw new Error('Invalid task definition: valueFrom format must be in the form of <service>:<path of variable>');
+            }
+            element.valueFrom = "arn:aws:".concat(valueFrom[0], ":::", "region name", ":", "accountID", ":", valueFrom[1])
+        });
+    }
 
     // Write out a new task definition file
     var updatedTaskDefFile = tmp.fileSync({
