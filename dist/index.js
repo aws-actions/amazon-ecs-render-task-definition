eebe84274a9e1aba8590dac5b8291dabc3ecda55
Pierre Wehbe <pierre.elie.wehbe@gmail.com>
2022-11-04 19:00:55 -0700

    feat: support for setting environment secrets, and other task-def params

--- dist/index.js
+++ dist/index.js
@@ -1190,10 +1190,20 @@ async function run() {
   try {
     // Get inputs
     const taskDefinitionFile = core.getInput('task-definition', { required: true });
-    const containerName = core.getInput('container-name', { required: true });
+
+    const family = core.getInput('family', { required: false });
+    const cpu = core.getInput('cpu', { required: false });
+    const memory = core.getInput('memory', { required: false });
+    const executionRoleArn = core.getInput('executionRoleArn', { required: false });
+    const taskRoleArn = core.getInput('taskRoleArn', { required: false });
+    
+    const containerName = core.getInput('container-name', { required: false });
     const imageURI = core.getInput('image', { required: true });
+    const awslogsGroup = core.getInput('awslogs-group', { required: false });
+    const awslogsRegion = core.getInput('awslogs-region', { required: false });
 
     const environmentVariables = core.getInput('environment-variables', { required: false });
+    const environmentSecrets = core.getInput('environment-secrets', { required: false });
 
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
@@ -1215,6 +1225,26 @@ async function run() {
       throw new Error('Invalid task definition: Could not find container definition with matching name');
     }
     containerDef.image = imageURI;
+
+    if (family) {
+      taskDefContents.family = family;
+    }
+
+    if (cpu) {
+      taskDefContents.cpu = cpu;
+    }
+
+    if (memory) {
+      taskDefContents.memory = memory;
+    }
+
+    if (executionRoleArn) {
+      taskDefContents.executionRoleArn = executionRoleArn;
+    }
+
+    if (taskRoleArn) {
+      taskDefContents.taskRoleArn = taskRoleArn;
+    }
 
     if (environmentVariables) {
 
@@ -1253,6 +1283,47 @@ async function run() {
       })
     }
 
+    if (environmentSecrets) {
+
+      // If environment array is missing, create it
+      if (!Array.isArray(containerDef.secrets)) {
+        containerDef.secrets = [];
+      }
+
+      // Get pairs by splitting on newlines
+      environmentSecrets.split('\n').forEach(function (line) {
+        // Trim whitespace
+        const trimmedLine = line.trim();
+        // Skip if empty
+        if (trimmedLine.length === 0) { return; }
+        // Split on =
+        const separatorIdx = trimmedLine.indexOf("=");
+        // If there's nowhere to split
+        if (separatorIdx === -1) {
+            throw new Error(`Cannot parse the environment secret '${trimmedLine}'. Environment secret pairs must be of the form KEY=value.`);
+        }
+        // Build object
+        const secret = {
+          name: trimmedLine.substring(0, separatorIdx),
+          valueFrom: trimmedLine.substring(separatorIdx + 1),
+        };
+
+        // Search container definition environment for one matching name
+        const variableDef = containerDef.secrets.find((e) => e.name == secret.name);
+        if (variableDef) {
+          // If found, update
+          variableDef.valueFrom = secret.valueFrom;
+        } else {
+          // Else, create
+          containerDef.secrets.push(secret);
+        }
+      })
+    }
+
+    if (awslogsGroup && awslogsRegion && containerDef.logConfiguration && containerDef.logConfiguration.options) {
+      containerDef.logConfiguration.options["awslogs-group"] = awslogsGroup;
+      containerDef.logConfiguration.options["awslogs-region"] = awslogsRegion;
+    }
 
     // Write out a new task definition file
     var updatedTaskDefFile = tmp.fileSync({
