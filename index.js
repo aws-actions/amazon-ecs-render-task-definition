c6f3dfbdd9990283e47c9e0cf46393ea13f1541b
Leonardo Silveira <sombriks@gmail.com>
2023-09-06 15:14:41 -0300

    feat: ability to configure log driver and log options (#255)
    * hability to configure log driver
    
    * changes from code review
    
    * changes from code review

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ async function run() {
 
     const environmentVariables = core.getInput('environment-variables', { required: false });
 
+    const logConfigurationLogDriver = core.getInput("log-configuration-log-driver", { required: false });
+    const logConfigurationOptions = core.getInput("log-configuration-options", { required: false });
+
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
       taskDefinitionFile :
@@ -25,7 +28,7 @@ async function run() {
     if (!Array.isArray(taskDefContents.containerDefinitions)) {
       throw new Error('Invalid task definition format: containerDefinitions section is not present or is not an array');
     }
-    const containerDef = taskDefContents.containerDefinitions.find(function(element) {
+    const containerDef = taskDefContents.containerDefinitions.find(function (element) {
       return element.name == containerName;
     });
     if (!containerDef) {
@@ -50,7 +53,7 @@ async function run() {
         const separatorIdx = trimmedLine.indexOf("=");
         // If there's nowhere to split
         if (separatorIdx === -1) {
-            throw new Error(`Cannot parse the environment variable '${trimmedLine}'. Environment variable pairs must be of the form NAME=value.`);
+          throw new Error(`Cannot parse the environment variable '${trimmedLine}'. Environment variable pairs must be of the form NAME=value.`);
         }
         // Build object
         const variable = {
@@ -70,6 +73,29 @@ async function run() {
       })
     }
 
+    if (logConfigurationLogDriver) {
+      if (!containerDef.logConfiguration) { containerDef.logConfiguration = {} }
+      const validDrivers = ["json-file", "syslog", "journald", "logentries", "gelf", "fluentd", "awslogs", "splunk", "awsfirelens"];
+      if (!validDrivers.includes(logConfigurationLogDriver)) {
+        throw new Error(`'${logConfigurationLogDriver}' is invalid logConfigurationLogDriver. valid options are ${validDrivers}. More details: https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_LogConfiguration.html`)
+      }
+      containerDef.logConfiguration.logDriver = logConfigurationLogDriver
+    }
+
+    if (logConfigurationOptions) {
+      if (!containerDef.logConfiguration) { containerDef.logConfiguration = {} }
+      if (!containerDef.logConfiguration.options) { containerDef.logConfiguration.options = {} }
+      logConfigurationOptions.split("\n").forEach(function (option) {
+        option = option.trim();
+        if (option && option.length) { // not a blank line
+          if (option.indexOf("=") == -1) {
+            throw new Error(`Can't parse logConfiguration option ${option}. Must be in key=value format, one per line`);
+          }
+          const [key, value] = option.split("=");
+          containerDef.logConfiguration.options[key] = value
+        }
+      })
+    }
 
     // Write out a new task definition file
     var updatedTaskDefFile = tmp.fileSync({
@@ -92,5 +118,5 @@ module.exports = run;
 
 /* istanbul ignore next */
 if (require.main === module) {
-    run();
+  run();
 }
