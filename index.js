6a9e92f84e1946309e450743d75b32ce76fcd59e
Evan Haston <ehaston@darkmatterit.io>
2024-04-24 17:06:17 -0400

    feat: Add support for task and execution role ARNs in action.yml and index.js

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ async function run() {
     const logConfigurationOptions = core.getInput("log-configuration-options", { required: false });
     const dockerLabels = core.getInput('docker-labels', { required: false });
     const command = core.getInput('command', { required: false });
+    const taskRoleArn = core.getInput('task-role-arn', { required: false });
+    const executionRoleArn = core.getInput('execution-role-arn', { required: false });
+
 
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
@@ -37,6 +40,8 @@ async function run() {
       throw new Error('Invalid task definition: Could not find container definition with matching name');
     }
     containerDef.image = imageURI;
+    containerDef.executionRoleArn = taskRoleArn;
+    containerDef.taskRoleArn = executionRoleArn
 
     if (command) {
       containerDef.command = command.split(' ')
